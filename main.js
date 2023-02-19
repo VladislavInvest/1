4905c96ad8f152cfd61a943f1c09af3259847507
VladislavInvest <gobkabob5000@gmail.com>
2023-02-19 12:01:49 +0300

    added privet

---
+++ main.js
@@ -0,0 +1,15 @@
+document.title = "сравнение чисел"
+
+function areRectangle(){
+    var a = document.forma.t1.value;
+    var b = document.forma.t2.value;
+    var S;
+    if  (S = a < b)
+    document.forma.res.value = b
+    else {
+    document.forma.res.value = a
+
+    }
+} 
+
+
